8dfc34a623b6f4ddefb6217427ba2c88612824f5
Sam <asamadans@gmail.com>
2023-06-07 02:05:42 +0530

    learnt about deadlock, producer-consumer pattern using pipeline and threads.

---
+++ deadlock.py
@@ -0,0 +1,10 @@
+import threading
+
+l = threading.Lock()
+print("before first acquire")
+l.acquire()
+print("before second acquire\nNow, stuck because the lock is already acquired (not released yet))")
+l.acquire()
+print("acquired lock twice")
+
+# To avoid deadlocks, never acquire more than one lock at a time, or you can use a RLocl (reentrant lock) instead of a Lock (non-reentrant lock)
